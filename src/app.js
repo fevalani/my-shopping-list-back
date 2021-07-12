17f628274c7772f53e3a8576a2250f050375c905
Fernando Valani Cavalcante <valanife@gmail.com>
2021-07-12 14:49:16 -0300

    create post items

--- src/app.js
+++ src/app.js
@@ -7,4 +7,16 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.post("/items", async (req, res) => {
+  try {
+    if (req.body.text?.length === 0) {
+      res.sendStatus(400);
+    }
+    await connection(`INSERT INTO list (text) VALUES ($1)`, [req.body.text]);
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(500);
+  }
+});
+
 export default app;
